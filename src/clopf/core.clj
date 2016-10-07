383e2ffa890821bb6206eddd46427c007dfd956e
Oliver Vartiainen <firoxer@gmail.com>
2016-10-08 01:46:43 +0300

    Tidy code

--- src/clopf/core.clj
+++ src/clopf/core.clj
@@ -2,37 +2,36 @@
 
 (defn destruct-packet
   "Validate the structure of a given byte array"
-  [structure bindings packet]
-  (defn byte-array->str [a] (apply str (map char a)))
-  (let [[id length kind] (first structure)
-        packet-is-done (or (nil? length) (< (count packet) length))]
-    (cond
-      (and packet-is-done (not-empty structure)) nil
-      (not-empty structure)
-        (let [new-binding
-               (case kind
-                 :string {id (byte-array->str (subvec packet 0 length))})]
-          (destruct-packet
-            (rest structure)
-            (conj bindings new-binding)
-            (subvec packet length)))
-      :else bindings)))
-
-(defn too-long?
-  [message]
-  (< 5 (count message)))
+  ([structure bindings packet]
+    (defn byte-array->str [a] (apply str (map char a)))
+    (let [[id length kind] (first structure)
+          packet-is-done (or (nil? length) (< (count packet) length))]
+      (cond
+        (and packet-is-done (not-empty structure)) nil
+        (not-empty structure)
+          (let [new-binding
+                 (case kind
+                   :string {id (byte-array->str (subvec packet 0 length))})]
+            (destruct-packet
+              (rest structure)
+              (conj bindings new-binding)
+              (subvec packet length)))
+        :else bindings)))
+  ([structure bindings]
+    (partial destruct-packet structure bindings)))
 
 (defn drop-if
-  [condition]
-  (fn [message]
-    (if-not (condition message)
-             message)))
+  ([condition destructured-packet]
+    (if-not (condition destructured-packet)
+             destructured-packet))
+  ([condition]
+    (partial drop-if condition)))
 
 (defn packet-handler
   "Make a function that takes a packet and runs it through the provided handlers"
   [& handlers]
   (fn [packet]
-    ((apply comp handlers) packet)))
+    ((apply comp (reverse handlers)) packet)))
 
 (defmacro defpacket
   "Define how a packet should be handled"
