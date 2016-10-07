e12dfda232001bb706afc26cdc0ee640bd2d951e
Oliver Vartiainen <firoxer@gmail.com>
2016-10-07 21:05:15 +0300

    Add basic building blocks for the DSL
    They don't do much yet

--- src/clopf/core.clj
+++ src/clopf/core.clj
@@ -1,6 +1,22 @@
 (ns clopf.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn too-long?
+  [message]
+  (< 5 (count message)))
+
+(defn drop-if
+  [condition]
+  (fn [message]
+    (if-not (condition message)
+             message)))
+
+(defn packet-handler
+  "Make a function that takes a packet and runs it through the provided handlers"
+  [& handlers]
+  (fn [packet]
+    ((apply comp handlers) packet)))
+
+(defmacro defpacket
+  "Define how a packet should be handled"
+  [name & handlers]
+  `(def ~name (packet-handler ~@handlers)))
