5a9a4d7341d21c284c7f3fd9f566be8c101aa8aa
Oliver Vartiainen <firoxer@gmail.com>
2016-10-07 19:29:48 +0300

    Lay out project structure using Lein

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clopf "0.1.0-SNAPSHOT"
+  :description "Clojure packet filtering"
+  :url "https://github.com/tkriik/clopf"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
