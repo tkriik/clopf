d51a93923e1044cbef445843723c303f4e0ec085
Oliver Vartiainen <firoxer@gmail.com>
2016-10-08 01:46:51 +0300

    Add tests for defpacket

--- test/clopf/core_test.clj
+++ test/clopf/core_test.clj
@@ -14,3 +14,15 @@
           bindings {}
           packet [(byte 0x61) (byte 0x62) (byte 0x63)]]
       (is (nil? (c/destruct-packet structure bindings packet))))))
+
+(deftest test-defpacket
+  (testing "ordinary packet"
+    (let [structure [[:id 3 :string]]
+          bindings {}
+          bad-packet [(byte 0x61) (byte 0x62) (byte 0x63)] ;; "abc"
+          good-packet [(byte 0x62) (byte 0x63) (byte 0x64)]] ;; "bcd"
+      (c/defpacket test-packet
+        (c/destruct-packet structure bindings)
+        (c/drop-if #(= (:id %) "abc")))
+      (is (nil? (test-packet bad-packet)))
+      (is (not (nil? (test-packet good-packet)))))))
